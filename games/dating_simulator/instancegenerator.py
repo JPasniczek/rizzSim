aaaa53f0b42d75e6c503006ab1a906ae4cba25b4
Imge Yüzüncüoglu <86414587+imgeyuez@users.noreply.github.com>
2024-06-02 19:08:41 +0200

    Update instancegenerator.py
    Pain and suffering :)

--- games/dating_simulator/instancegenerator.py
+++ games/dating_simulator/instancegenerator.py
@@ -1,6 +1,37 @@
 import os
 import json
+import clemgame
+from clemgame.clemgame import GameInstanceGenerator
 
+
+
+class GameInstanceGenerator(GameResourceLocator):
+
+    # include the self.load_template() and self.load_file() for initial prompt and stuff 
+    
+    def __init__(self, name: str):
+        super().__init__(name)
+        self.instances = dict(experiments=list())
+    
+    def on_generate(self, **kwargs):
+        random.seed(42)
+        
+        """
+        Game-specific instance generation.
+        """
+        
+        player_char_prompt = self.load_template("path to the initial prompt aka start_prompt")
+
+        # add experiment 
+        # add instance 
+        
+        raise NotImplementedError()
+
+if __name__ == '__main__':
+    ReferenceGameInstanceGenerator().generate(filename="instances.json")
+
+
+#######################################################################
 # generate JSON file to save playthrough of game
 save_dir = "./playthroughs/"
 if not os.path.exists(save_dir):
@@ -14,3 +45,4 @@ playthrough_json = f"./playthroughs/{nr_of_plays+1}_playthrough.json"
 with open(playthrough_json, "w", encoding="UTF-8") as file:
     current_play_json = json.load(file)
 print(current_play_json)
+
